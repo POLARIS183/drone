1e54e8aa9f20af5fb6c13da640cfe58818ec5d69
POLARIS183 <68078955+POLARIS183@users.noreply.github.com>
2022-01-14 16:51:29 +0000

    Added win-install.bat

---
+++ win-install.bat
@@ -0,0 +1,8 @@
+curl https://www.python.org/ftp/python/3.10.1/python-3.10.1-amd64.exe
+python-3.10.1-amd64.exe
+pause
+curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+python get-pip.py
+pause
+pip install djitellopy
+echo DJItelloPY has now been installed.
